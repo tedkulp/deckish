e878e6e2695883629d50953a8b717e3ff5f640a5
Ted Kulp <ted@tedkulp.com>
2017-12-09 20:38:01 -0500

    Ability to switch layouts while holding a key

--- index.js
+++ index.js
@@ -2,83 +2,106 @@
 const {obs, getScene, getSceneName, setScene, setPreviousScene, toggleSceneItem} = require('./lib/obs');
 const {streamDeck, convertKey} = require('./lib/stream_deck');
 
-const mainLayout = [
-    [
-        {
-            type: 'switchScene',
-            sceneName: 'Full Screen HDMI',
-        },
-        {
-            type: 'switchScene',
-            sceneName: 'Full Desktop Camera',
-        },
-        {
-            type: 'switchScene',
-            sceneName: 'Polaroid',
-        },
-        {
-            type: 'switchScene',
-            sceneName: 'Keyboard Cam',
-        },
-        {
-            type: 'toggleScene',
-            sceneName: 'BRB',
-        },
-    ],
-    [
-        {
-            type: 'switchScene',
-            sceneName: 'Local Browser',
-        },
-        {
-            type: '',
-        },
-        {
-            type: '',
-        },
-        {
-            type: '',
-        },
-        {
-            type: 'momentaryScene',
-            sceneName: 'ZOOM!',
-        },
-    ],
-    [
-        {
-            type: 'sceneSourceToggle',
-            scenes: {
-                'Full Screen HDMI': 'Face Camera Scene - Green Screen',
-                'Polaroid': 'Face Camera Scene 2 - Colours',
+const layouts = {
+    main: [
+        [
+            {
+                type: 'switchScene',
+                sceneName: 'Full Screen HDMI',
             },
-        },
-        {
-            type: 'sceneSourceToggle',
-            scenes: {
-                'Full Screen HDMI': 'Keyboard Camera Scene',
+            {
+                type: 'switchScene',
+                sceneName: 'Full Desktop Camera',
             },
-        },
-        {
-            type: '',
-        },
-        {
-            type: '',
-        },
-        {
-            type: '',
-        },
+            {
+                type: 'switchScene',
+                sceneName: 'Polaroid',
+            },
+            {
+                type: 'switchScene',
+                sceneName: 'Keyboard Cam',
+            },
+            {
+                type: 'toggleScene',
+                sceneName: 'BRB',
+            },
+        ],
+        [
+            {
+                type: 'switchScene',
+                sceneName: 'Local Browser',
+            },
+            {
+                type: 'debug',
+                message: "I'm a button on the main layout",
+            },
+            {
+                type: '',
+            },
+            {
+                type: '',
+            },
+            {
+                type: 'momentaryScene',
+                sceneName: 'ZOOM!',
+            },
+        ],
+        [
+            {
+                type: 'sceneSourceToggle',
+                scenes: {
+                    'Full Screen HDMI': 'Face Camera Scene - Green Screen',
+                    'Polaroid': 'Face Camera Scene 2 - Colours',
+                },
+            },
+            {
+                type: 'sceneSourceToggle',
+                scenes: {
+                    'Full Screen HDMI': 'Keyboard Camera Scene',
+                },
+            },
+            {
+                type: '',
+            },
+            {
+                type: '',
+            },
+            {
+                type: 'momentaryLayout',
+                layoutName: 'sounds',
+            },
+        ],
     ],
-];
+    sounds: [
+        [
+            {
+                type: 'debug',
+                message: "I'm a button on the sounds layout",
+            },
+        ],
+    ]
+};
+
+let currentLayout = 'main';
+let previousLayout = currentLayout;
+let currentMomentaryButton = null;
 
 streamDeck.on('down', keyIndex => {
     const {row, col} = convertKey(keyIndex);
     console.log('key down row: ', row, "col:", col);
 
-    const keyFound = mainLayout[row] && mainLayout[row][col];
+    const keyFound = layouts[currentLayout][row] && layouts[currentLayout][row][col];
     if (keyFound && keyFound.type) {
         switch(keyFound.type) {
             case 'momentaryScene':
                 setScene(keyFound.sceneName);
+                break;
+            case 'momentaryLayout':
+                if (keyFound.layoutName && layouts[keyFound.layoutName]) {
+                    previousLayout = currentLayout;
+                    currentLayout = keyFound.layoutName;
+                    currentMomentaryButton = {key: keyFound, x: col, y: row};
+                }
                 break;
         }
     }
@@ -90,9 +113,14 @@ streamDeck.on('up', keyIndex => {
     const {row, col} = convertKey(keyIndex);
     console.log('key up row: ', row, "col:", col);
 
-    const keyFound = mainLayout[row] && mainLayout[row][col];
+    const keyFound = (currentMomentaryButton && currentMomentaryButton.x === col && currentMomentaryButton.y === row && currentMomentaryButton.key) ||
+        (layouts[currentLayout][row] && layouts[currentLayout][row][col]);
     if (keyFound && keyFound.type) {
         switch(keyFound.type) {
+            case 'debug':
+                if (keyFound.message)
+                    console.log(keyFound.message);
+                break;
             case 'switchScene':
                 sceneToggled = false;
                 setScene(keyFound.sceneName);
@@ -115,6 +143,13 @@ streamDeck.on('up', keyIndex => {
                         const sceneItemName = keyFound.scenes[currentSceneName];
                         toggleSceneItem(currentSceneName, sceneItemName);
                     }
+                }
+                break;
+            case 'momentaryLayout':
+                currentMomentaryButton = null;
+                if (keyFound.layoutName && keyFound.layoutName === currentLayout) {
+                    currentLayout = previousLayout;
+                    previousLayout = keyFound.layoutName;
                 }
                 break;
         }
--- notes.markdown
+++ notes.markdown
@@ -1,6 +1,6 @@
 # <strike>Toggle scene - put it back to previous scene when button is hit again</strike>
 # <strike>Scene dependent keys for toggling sources</strike>
-# Play sounds
-# Ability to hold button to change layouts
-# Set images for buttons
 # <strike>Make sure setScene promise is caught if there is an error</strike>
+# <strike>Ability to hold button to change layouts</strike>
+# Set images for buttons
+# Play sounds
