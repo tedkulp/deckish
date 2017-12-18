3d179fccb6df5c9644214ab833bbcd8f4cc92384
Ted Kulp <ted@tedkulp.com>
2017-12-18 06:42:22 -0500

    Updates from 2017-12-18 stream
    - Added redux-watch
    - Start setting up button rendering
    - Changes es2015 to env (off-stream)

--- layouts.js
+++ layouts.js
@@ -1,4 +1,6 @@
+import watch from 'redux-watch'
 import store from './lib/store';
+const { convertKey } = require('./lib/stream_deck');
 
 const layouts = {
     main: [
@@ -6,22 +8,72 @@ const layouts = {
             {
                 type: 'switchScene',
                 sceneName: 'Full Screen HDMI',
+                visual: {
+                    untoggled: {
+                        type: 'color',
+                        color: 'green',
+                    },
+                    toggled: {
+                        type: 'color',
+                        color: 'red',
+                    },
+                },
             },
             {
                 type: 'switchScene',
                 sceneName: 'Full Desktop Camera',
+                visual: {
+                    untoggled: {
+                        type: 'color',
+                        color: 'green',
+                    },
+                    toggled: {
+                        type: 'color',
+                        color: 'red',
+                    },
+                },
             },
             {
                 type: 'switchScene',
                 sceneName: 'Polaroid',
+                visual: {
+                    untoggled: {
+                        type: 'color',
+                        color: 'green',
+                    },
+                    toggled: {
+                        type: 'color',
+                        color: 'red',
+                    },
+                },
             },
             {
                 type: 'switchScene',
                 sceneName: 'Keyboard Cam',
+                visual: {
+                    untoggled: {
+                        type: 'color',
+                        color: 'green',
+                    },
+                    toggled: {
+                        type: 'color',
+                        color: 'red',
+                    },
+                },
             },
             {
                 type: 'toggleScene',
                 sceneName: 'BRB',
+                visual: {
+                    untoggled: {
+                        type: 'color',
+                        color: 'green',
+                    },
+                    toggled: {
+                        type: 'color',
+                        color: 'red',
+                    },
+                },
             },
         ],
         [
@@ -84,5 +136,28 @@ const layouts = {
 };
 
 store.dispatch({ type: 'INIT_LAYOUTS', value: layouts });
+
+const updateButtonState = (newVal, oldVal, objectPath) => {
+    console.log('%s changed from %s to %s', objectPath, oldVal, newVal);
+
+    let layout = store.getState().layouts[store.getState().currentLayout];
+
+    for (let idx = 0; idx < 15; idx++) {
+        const { row, col } = convertKey(idx);
+        let key = layout[row][col];
+        if (key && key.visual) {
+            if (key.visual.untoggled) {
+                store.dispatch({ type: 'SET_BUTTON', index: idx, value: key.visual.untoggled });
+            }
+        }
+    }
+};
+
+
+let w = watch(store.getState, 'currentLayout');
+store.subscribe(w(updateButtonState));
+
+let { currentLayout, previousLayout } = store.getState();
+updateButtonState(currentLayout, previousLayout, 'currentLayout');
 
 export default layouts;
